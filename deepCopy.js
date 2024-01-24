e10415187450961a204cdf6ef6c2848f2fc0c161
youmeeeee <youmechoi1129@gmail.com>
2024-01-24 10:25:38 +0900

    edited deep copy function

--- deepCopy.js
+++ deepCopy.js
@@ -1,8 +1,8 @@
 const deepCopy = (obj) => {
     if (Array.isArray(obj)) {
        return  newObj = obj.map(item => deepCopy(item));
-    } else if (typeof obj === 'object' && obj !== null) {
-        const newObj = {};
+    } else if (typeof obj === 'object' && obj !== null) { //null의 type도 object이므로!
+        const newObj = {};  
         for (let key in obj) {
             newObj[key] = deepCopy(obj[key])
         }
